f93fb634dc1028e5151421d1f68a3784de73e301
spences10 <spences10apps@gmail.com>
2022-12-28 21:29:24 +0000

    feat: :sparkles: add logout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,3 +3,7 @@
 	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the
 	documentation
 </p>
+
+<form action="/logout" method="POST">
+	<button type="submit" class="btn btn-primary ">Logout</button>
+</form>
