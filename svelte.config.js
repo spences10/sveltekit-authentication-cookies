08ab827893ceafed0e9c1748e25b37bb82450d09
spences10 <spences10apps@gmail.com>
2022-12-29 13:30:22 +0000

    refactor: :recycle: use adapter vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
-import preprocess from 'svelte-preprocess'
-import adapter from '@sveltejs/adapter-auto'
+import adapter from '@sveltejs/adapter-vercel'
 import { vitePreprocess } from '@sveltejs/kit/vite'
+import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
