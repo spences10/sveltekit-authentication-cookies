7e714d7a2c91e690baf37cba9fe9d2a2136b431d
spences10 <spences10apps@gmail.com>
2023-01-02 10:22:39 +0000

    refactor: :recycle: add user register validations

--- src/lib/schemas.ts
+++ src/lib/schemas.ts
@@ -6,3 +6,44 @@ export const loginUserSchema = z.object({
 		.email({ message: `Email is invalid` }),
 	password: z.string({ required_error: `Password is required` }),
 })
+
+export const registerUserSchema = z
+	.object({
+		name: z
+			.string({ required_error: `Name is required` })
+			.regex(/^[a-zA-Z ]+$/, {
+				message: `Name can only contain letters and spaces`,
+			})
+			.min(2, { message: `Name must be at least 2 characters` })
+			.max(64, {
+				message: `Name must be less than 64 characters`,
+			})
+			.trim(),
+		email: z.string({ required_error: `Email is required` }).email({
+			message: `Email is invalid`,
+		}),
+		password: z
+			.string({ required_error: `Password is required` })
+			.regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/, {
+				message: `Password must be at least 8 characters and contain at least one uppercase letter, one lowercase letter, and one number`,
+			}),
+		passwordConfirm: z
+			.string({ required_error: `Please confirm password` })
+			.regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/, {
+				message: `Password must be at least 8 characters and contain at least one uppercase letter, one lowercase letter, and one number`,
+			}),
+	})
+	.superRefine(({ passwordConfirm, password }, ctx) => {
+		if (passwordConfirm !== password) {
+			ctx.addIssue({
+				code: z.ZodIssueCode.custom,
+				message: 'Password and Confirm Password must match',
+				path: ['password'],
+			})
+			ctx.addIssue({
+				code: z.ZodIssueCode.custom,
+				message: 'Password and Confirm Password must match',
+				path: ['passwordConfirm'],
+			})
+		}
+	})
--- src/routes/(auth)/register/+page.server.ts
+++ src/routes/(auth)/register/+page.server.ts
@@ -1,22 +1,33 @@
-import { generateUsername } from '$lib/utils'
-import { error, redirect } from '@sveltejs/kit'
+import { registerUserSchema } from '$lib/schemas'
+import { generateUsername, validateData } from '$lib/utils'
+import { error, fail, redirect } from '@sveltejs/kit'
 import type { Actions } from './$types'
 
 export const actions: Actions = {
 	register: async ({ locals, request }) => {
-		const body = Object.fromEntries(await request.formData())
+		const { formData, errors } = await validateData(
+			await request.formData(),
+			registerUserSchema
+		)
+
+		if (errors) {
+			return fail(400, {
+				data: formData,
+				errors: errors.fieldErrors,
+			})
+		}
 
 		let username = generateUsername(
-			body.name.toString().split(' ').join('')
+			formData.name.toString().split(' ').join('')
 		).toLowerCase()
 
 		try {
 			await locals.pb
 				.collection('users')
-				.create({ emailVisibility: false, username, ...body })
+				.create({ emailVisibility: false, username, ...formData })
 			await locals.pb
 				.collection('users')
-				.requestVerification(body.email.toString())
+				.requestVerification(formData.email.toString())
 		} catch (err) {
 			console.log(`Error: ${err}`)
 			throw error(500, 'Something went wrong')
